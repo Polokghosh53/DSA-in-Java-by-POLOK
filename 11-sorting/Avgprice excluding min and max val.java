66232aa3b423d4b41d312c57d3490ade1f8eeead
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2021-10-27 20:44:45 +0530

    Create Avgprice excluding min and max val.java

---
+++ 11-sorting/Avgprice excluding min and max val.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double average(int[] sal) {
+        int min = sal[0];
+        int max = sal[0];
+        
+        for (int i = 0; i < sal.length; i++){
+            if(sal[i] < min){
+                min = sal[i];
+            }
+            else if(sal[i] > max){
+                max = sal[i];
+            }
+        }
+        double sum = 0;
+        for(int i = 0; i < sal.length; i++){
+            sum += sal[i];
+        }
+        
+        double ans = (sum - (min + max))/(sal.length - 2);
+        return ans;
+ }
+}

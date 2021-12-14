4a023e1670fb331c56a30118d91680dd77b20354
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2021-12-14 18:30:07 +0530

    Create isPowerOfTwo.java

---
+++ 14-recursion/isPowerOfTwo.java
@@ -0,0 +1,16 @@
+package com.company;
+// https://leetcode.com/problems/power-of-two/
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n == 0){
+            return false;
+        }
+        if(n == 1){
+            return true;
+        }
+        if(n % 2 == 1){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+    }
+}

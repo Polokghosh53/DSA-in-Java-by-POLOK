e0cc1d9cecef2800a618be4ddb354ad910502d58
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-01-26 18:55:08 +0530

    Create BinaryGap.java

---
+++ 15-MathsforDSA/BinaryGap.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/binary-gap/
+class Solution {
+    public int binaryGap(int n) {
+        int last = -1, ans = 0;
+        for(int i = 0; i < 32; i++){
+            if(((n >> i) & 1) > 0){
+                if(last >= 0){
+                    ans = Math.max(ans, i - last);
+                }
+                last = i;
+            }
+        }
+        return ans;
+    }
+}

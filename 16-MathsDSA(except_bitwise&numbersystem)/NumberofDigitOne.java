6f57e10abcdb93c73e612864bd6b4a6a9a4e33d9
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-03-15 12:13:37 +0530

    Create NumberofDigitOne.java

---
+++ 16-MathsDSA(except_bitwise&numbersystem)/NumberofDigitOne.java
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/number-of-digit-one/
+class Solution {
+    public int countDigitOne(int n) {
+        int ones = 0;
+        for(long m = 1; m <= n; m *= 10){
+            ones += (n / m + 8) / 10 * m + (n / m % 10 == 1 ? n % m + 1 : 0);
+        }
+        return ones;
+    }
+}

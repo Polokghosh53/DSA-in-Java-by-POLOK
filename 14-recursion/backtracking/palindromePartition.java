f1a138b04266665c2ae1440bb76c9e6b4fb3bb7a
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-06-23 12:58:09 +0530

    Update palindromePartition.java

--- 14-recursion/backtracking/palindromePartition.java
+++ 14-recursion/backtracking/palindromePartition.java
@@ -17,25 +17,28 @@ public class Main
     }
     
     public static void backtrack(List<List<String>> list, List<String> tempList, String s, int start) {
-        if (start == s.length()) {
-            list.add(new ArrayList<>(tempList));
-        } else {
-            for (int i = start; i < s.length(); i++) {
-                if (isPalindrome(s, start, i)) {
-                    tempList.add(s.substring(start, i + 1));
-                    backtrack(list, tempList, s, i + 1);
-                    tempList.remove(tempList.size() - 1);
-                }
-            }
-        }
-    }
-    
-    public static boolean isPalindrome(String s, int low, int end) {
-        while(low < end) {
-            if(s.charAt(low++) != s.charAt(end--)) {
-                return false;
-            }
-        }
-        return true;
-    }
+	    if (start == s.length()) {
+	        // add the list of lists
+	        list.add(new ArrayList<>(tempList));
+	    }
+	    else {
+	        for (int i = start; i < s.length(); i++) {
+	            if (isPalindrome(s, start, i)) {
+	                // adding a char in the temporary list
+    	            tempList.add(s.substring(start, i + 1));
+    	            backtrack(list, tempList, s, i + 1);
+    	            tempList.remove(tempList.size() - 1);
+	            }
+	        }
+	    }
+	}
+	
+	public static boolean isPalindrome(String s, int low, int high) {
+	    while(low < high) {
+	        if (s.charAt(low++) != s.charAt(high--)) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
 }

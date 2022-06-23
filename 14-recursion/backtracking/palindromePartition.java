f1fe196d29f7a2fbd6ad0a6ba1f9192fe4d79294
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-06-23 12:42:34 +0530

    Create palindromePartition.java

---
+++ 14-recursion/backtracking/palindromePartition.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		String s = "nitin";
+		System.out.println(partition(s));
+	}
+	
+	public static List<List<String>> partition(String s) {
+        List<List<String>> list = new ArrayList<>();
+        backtrack(list, new ArrayList<>(), s, 0);
+        return list;
+    }
+    
+    public static void backtrack(List<List<String>> list, List<String> tempList, String s, int start) {
+        if (start == s.length()) {
+            list.add(new ArrayList<>(tempList));
+        } else {
+            for (int i = start; i < s.length(); i++) {
+                if (isPalindrome(s, start, i)) {
+                    tempList.add(s.substring(start, i + 1));
+                    backtrack(list, tempList, s, i + 1);
+                    tempList.remove(tempList.size() - 1);
+                }
+            }
+        }
+    }
+    
+    public static boolean isPalindrome(String s, int low, int end) {
+        while(low < end) {
+            if(s.charAt(low++) != s.charAt(end--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

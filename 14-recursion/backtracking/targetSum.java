555f90ba7101cb4210b41557d6b0c772526accd5
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-06-24 12:26:40 +0530

    Create targetSum.java

---
+++ 14-recursion/backtracking/targetSum.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/target-sum/
+
+class Solution {
+    
+    int count = 0;
+    public int findTargetSumWays(int[] nums, int target) {
+        calculate(nums, target, 0, 0);
+        return count;
+    }
+    
+    public void calculate(int[] nums, int target, int i, int sum) {
+        if (i == nums.length) {
+            if (sum == target) {
+                count++;
+            }
+        } else {
+            calculate(nums, target, i + 1, sum + nums[i]);
+            calculate(nums, target, i + 1, sum - nums[i]);
+        }
+    }
+}

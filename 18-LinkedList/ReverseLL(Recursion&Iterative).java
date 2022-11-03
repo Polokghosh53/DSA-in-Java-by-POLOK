d80362358c89d0d49cc579b7652b9893dc400773
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-11-03 12:41:16 +0530

    Create ReverseLL(Recursion&Iterative).java

---
+++ 18-LinkedList/ReverseLL(Recursion&Iterative).java
@@ -0,0 +1,58 @@
+public class LL {
+   private Node head;
+   private Node tail;
+   private int size;
+   public LL() {
+       this.size = 0;
+   }
+  
+  private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+  
+  // RECURSIVE
+  private void reverse(Node node) {
+        if (node == tail) {
+            head = tail;
+            return;
+        }
+        // the following is done: 4->3->2->1
+        reverse(node.next);
+        // pointing 1 as tail and proceeding in reverse
+        // tail which is 1 then 1->2->3->4
+        tail.next = node;
+        tail = node;
+        tail.next = null;
+    }
+  
+  // ITERATIVE
+  public void reverse() {
+        if (size < 2) {
+            return;
+        }
+
+        Node prev = null;
+        Node present = head;
+        Node next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        head = prev;
+    }
+}

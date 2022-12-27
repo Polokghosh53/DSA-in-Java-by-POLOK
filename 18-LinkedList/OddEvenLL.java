1a01fb4229763c6f5c951292f948dcc7eba660e6
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-12-27 20:54:32 +0530

    Create OddEvenLL.java

---
+++ 18-LinkedList/OddEvenLL.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/odd-even-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return head;
+        
+        // using two-pointer method where head = odd index & head.next = even index
+        ListNode odd = head;
+        ListNode even = head.next;
+        
+        // temporary evenHead to connect the odd node with the even nodes 1->3->5 ---> 2->4
+        ListNode evenHead = even;
+        
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        
+        return head;
+    }
+}

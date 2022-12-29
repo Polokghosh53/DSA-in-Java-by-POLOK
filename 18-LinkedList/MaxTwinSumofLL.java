5ee01ed708f7993536236838bce50e60b3db730d
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2022-12-29 21:03:17 +0530

    Create MaxTwinSumofLL.java

---
+++ 18-LinkedList/MaxTwinSumofLL.java
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  // reverse the linked list from the middle and use two pointer to add two ends of the linked list and find maximum sum
+    public int pairSum(ListNode head) {
+        if (head == null) {
+            return head.val;
+        }
+        
+        if (head.next == null) {
+            return head.val;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        slow = reverse(slow);
+        fast = head;
+        int sum = Integer.MIN_VALUE;
+        while (slow != null) {
+            sum = Math.max(slow.val + fast.val, sum);
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return sum;
+    }
+    
+    public ListNode reverse(ListNode node) {
+        if (node == null) {
+            return node;
+        }
+        
+        ListNode curr = node;
+        ListNode prev = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}

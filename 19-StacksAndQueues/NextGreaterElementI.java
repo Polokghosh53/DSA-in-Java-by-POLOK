97fc92fc02df86c08cd272a2ec250a7ac899e2c8
Polok-Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-01-15 11:59:49 +0530

    Create NextGreaterElementI.java

---
+++ 19-StacksAndQueues/NextGreaterElementI.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/next-greater-element-i
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] result = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums2) {
+            while (!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        int i = 0;
+        for (int num : nums1) {
+            result[i++] = map.getOrDefault(num, -1);
+        }
+        return result;
+    }
+}

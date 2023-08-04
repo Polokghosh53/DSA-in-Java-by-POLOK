85004961e00f8946e7aae16546b61aca9108d4b4
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-08-04 20:22:01 +0530

    Update BinaryTree.java

--- 20-BinaryTree/BinaryTree.java
+++ 20-BinaryTree/BinaryTree.java
@@ -58,11 +58,32 @@ public class BinaryTree {
         display(node.left, indent + "\t");
         display(node.right, indent + "\t");
     }
+    
+    public void prettyDisplay() {
+        prettyDisplay(root, 0);
+    }
+
+    private void prettyDisplay(Node node, int level) {
+        if (node == null) {
+            return;
+        }
+        prettyDisplay(node.right, level + 1);
+
+        if (level != 0) {
+            for (int i = 0; i < level - 1; i++) {
+                System.out.print("|\t\t");
+            }
+            System.out.println("|------->" + node.value);
+        } else {
+            System.out.println(node.value);
+        }
+        prettyDisplay(node.left, level + 1);
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         BinaryTree tree = new BinaryTree();
         tree.populate(sc);
-        tree.display();
+        tree.prettyDisplay();
     }
 }

1cd9e8dddf1a1867e34d940e282f3b18804ae2ba
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-11-24 23:23:13 +0530

    Create levelOrderBottom.java

---
+++ 20-BinaryTree/levelOrderBottom.java
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        boolean reverse = false;
+
+        while(!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+               TreeNode currentNode = queue.poll();
+               currentLevel.add(currentNode.val);
+               if (currentNode.left != null) {
+                   queue.offer(currentNode.left);
+               }
+               if (currentNode.right != null) {
+                   queue.offer(currentNode.right);
+               }
+            }
+            result.add(0, currentLevel);
+        }
+        return result;
+    }
+}

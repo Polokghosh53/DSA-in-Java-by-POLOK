1755dc24b7cd77ea50a888d66b71789ecea21013
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-11-27 00:43:37 +0530

    Create Populating_NextRight_pointer.java

---
+++ 20-BinaryTree/Populating_NextRight_pointer.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+
+        Node leftMost = root;
+
+        while(leftMost.left != null) {
+            Node current = leftMost;
+            while (current != null) {
+                current.left.next = current.right;
+                if (current.next != null) {
+                    current.right.next = current.next.left;
+                }
+                current = current.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+}

f3eda8880dd8869a5a6842778f314d970e0524e7
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-11-27 23:23:19 +0530

    Create BT_RightSide_View.java

---
+++ 20-BinaryTree/BT_RightSide_View.java
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/binary-tree-right-side-view/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+
+                if (i == levelSize - 1) {
+                    result.add(currentNode.val);
+                }
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+        }
+        return result;
+    }
+}

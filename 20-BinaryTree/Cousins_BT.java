97f9e0a0ecc12fcb99d0d68407b9101edc913545
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-11-30 00:07:05 +0530

    Update Cousins_BT.java

--- 20-BinaryTree/Cousins_BT.java
+++ 20-BinaryTree/Cousins_BT.java
@@ -21,7 +21,7 @@ class Solution {
 
         return(
             (level(root,xx,0) == level(root,yy,0)) && (!isSibling(root,xx,yy))
-        )
+        );
     }
 
     TreeNode findNode(TreeNode node, int x) {
@@ -47,7 +47,24 @@ class Solution {
 
         return (
             (node.left == x && node.right == y) || (node.left == y && node.right == x)
-           || (isSibling(node.left, x, y) || (isSibling(node.right, x, y); 
-        )
+           || (isSibling(node.left, x, y)) || (isSibling(node.right, x, y))
+        );
+    }
+
+    int level(TreeNode node, TreeNode x, int lev) {
+        if (node == null) {
+            return 0;
+        }
+
+        if (node == x) {
+            return lev;
+        }
+
+        int l = level(node.left, x, lev+1);
+        if (l != 0) {
+            return l;
+        }
+
+        return level(node.right, x, lev+1);
     }
 }

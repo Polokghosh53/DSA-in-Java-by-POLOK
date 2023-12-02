4f16856be77ea4f34b0d37f16f88c30776566fac
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-03 00:39:41 +0530

    Update Diameter_BT.java

--- 20-BinaryTree/Diameter_BT.java
+++ 20-BinaryTree/Diameter_BT.java
@@ -1,1 +1,38 @@
 // https://leetcode.com/problems/diameter-of-binary-tree/
+// DFS
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        height(root);
+        return diameter-1; // calculating the length of edges
+    }
+
+    int height(TreeNode node) {
+        if(node == null) {
+            return 0;
+        }
+
+        int leftHeight = height(node.left);
+        int rightHeight = height(node.right);
+
+        int dia = leftHeight + rightHeight + 1;
+        diameter = Math.max(diameter, dia);
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}

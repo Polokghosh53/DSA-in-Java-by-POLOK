6b4c8823ce8eb54695828ee6dfe6191b232ed917
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-03 23:45:36 +0530

    Create MaxDepth_BT.java

---
+++ 20-BinaryTree/MaxDepth_BT.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int depth = 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        int depth = Math.max(left, right) + 1;
+
+        return depth;
+    }
+}

84c18b734ee1b60aa8b203ffeb2b66d64c37441e
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-06 23:32:35 +0530

    Create FlattenBT_to_LL.java

---
+++ 20-BinaryTree/FlattenBT_to_LL.java
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode current = root;
+        while (current != null) {
+            if (current.left != null) {
+                TreeNode temp = current.left;
+                while (temp.right != null) {
+                    temp = temp.right;
+                }
+                temp.right = current.right;
+                current.right = current.left;
+                current.left = null; 
+            }
+            current = current.right;
+        }
+    }
+}

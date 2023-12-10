4783101e65a3f5d2d3692abba3f207112c4d637f
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-10 23:55:41 +0530

    Create ConstructBT_fromPreOrderandInOrder.java

---
+++ 20-BinaryTree/ConstructBT_fromPreOrderandInOrder.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+
+        int r = preorder[0];
+        int index = 0;
+
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == r) {
+                index = i;
+            }
+        }
+
+        TreeNode node = new TreeNode(r);
+        node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
+        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
+
+        return node;
+    }
+}

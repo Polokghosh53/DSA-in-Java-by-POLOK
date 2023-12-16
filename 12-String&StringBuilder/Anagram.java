aa8ce7b2ef23f0594f464d2be15b6d2bee27f12f
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-16 23:48:55 +0530

    Create Anagram.java

---
+++ 12-String&StringBuilder/Anagram.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/valid-anagram/
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> counter = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            counter.put(ch, counter.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            if (!counter.containsKey(ch) || counter.get(ch) == 0) {
+                return false;
+            }
+            counter.put(ch, counter.get(ch) - 1);
+        }
+
+        return true;        
+    }
+}

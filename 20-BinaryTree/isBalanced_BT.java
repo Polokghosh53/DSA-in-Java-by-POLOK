7c67890c84641cb2ed06bd60d97b6f8c424efdd2
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-17 22:08:45 +0530

    Create isBalanced_BT.java

---
+++ 20-BinaryTree/isBalanced_BT.java
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        if (!isBalanced(root.left) || !isBalanced(root.right)) {
+            return false;
+        }
+
+        if (Math.abs(height(root.left) - height(root.right)) > 1) {
+            return false;
+        }
+        return true;
+    }
+
+    int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        
+        return Math.max(left, right) + 1;
+    }
+}

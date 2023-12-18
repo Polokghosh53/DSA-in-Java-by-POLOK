dcb765fd0bbe0c229e40b34677516f35a6d508df
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2023-12-18 23:33:40 +0530

    Create maxProductDiff.java

---
+++ 8-arrays/maxProductDiff.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int biggest = 0;
+        int secondBiggest = 0;
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        
+        for (int num : nums) {
+            if (num > biggest) {
+                secondBiggest = biggest;
+                biggest = num;
+            } else {
+                secondBiggest = Math.max(secondBiggest, num);
+            }
+            
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else {
+                secondSmallest = Math.min(secondSmallest, num);
+            }
+        }
+        
+        return biggest * secondBiggest - smallest * secondSmallest;
+    }
+}

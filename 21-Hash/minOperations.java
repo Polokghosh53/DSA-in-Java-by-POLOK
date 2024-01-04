9b00c15e880583b744af7f0d4ad93a7c1201f371
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2024-01-05 00:01:15 +0530

    Create minOperations.java

---
+++ 21-Hash/minOperations.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+class Solution {
+    public int minOperations(int[] nums) {
+        var counter = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            counter.put(num, counter.getOrDefault(num, 0) + 1);
+        }
+
+        int ans = 0;
+        for (int c : counter.values()) {
+            if (c == 1) {
+                return -1;
+            }
+            ans += Math.ceil((double)c / 3 );
+        }
+        return ans;
+    }
+}

d3662198e9dbf8e75268fd25126139cc128b470c
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2024-01-11 23:39:34 +0530

    Update amountofTime(VP).java

--- 20-BinaryTree/amountofTime(VP).java
+++ 20-BinaryTree/amountofTime(VP).java
@@ -1,4 +1,3 @@
-// https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -15,28 +14,27 @@
  * }
  */
 class Solution {
-    int ans = 0;
-    public int amountOfTime(TreeNode root, int start) {
-        dfs(root, start);
-        return ans;
+  private int ans;
+
+  public int amountOfTime(TreeNode root, int start) {
+    dfs(root, start);
+    return ans;
+  }
+
+  public int dfs(TreeNode root, int start) {
+    if (root == null) return 0;
+
+    int leftDepth = dfs(root.left, start);
+    int rightDepth = dfs(root.right, start);
+
+    if (root.val == start) {
+      ans = Math.max(leftDepth, rightDepth);
+      return -1;
+    } else if (leftDepth >= 0 && rightDepth >= 0) {
+      return Math.max(leftDepth, rightDepth) + 1;
+    } else {
+      ans = Math.max(ans, Math.abs(leftDepth - rightDepth));
+      return Math.min(leftDepth, rightDepth) - 1; //  if start is not root
     }
-
-    public int dfs(TreeNode root, int start) {
-        if (root == null) {
-            return 0;
-        }
-
-        int leftDepth = dfs(root.left, start);
-        int rightDepth = dfs(root.right, start);
-
-        if (root.val == start) {
-            ans = Math.max(leftDepth, rightDepth);
-            return -1;
-        } else if(leftDepth >= 0 && rightDepth >= 0) {
-            return Math.max(leftDepth, rightDepth) + 1;
-        } else {
-            ans = Math.max(ans, Math.abs(leftDepth - rightDepth));
-            return Math.max(leftDepth, rightDepth) - 1;
-        }
-    }
+  }
 }

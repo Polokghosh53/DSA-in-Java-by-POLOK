b6dbc5f9710efdd49077e0eb5f1a67fd3ba060bd
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2024-02-04 23:02:58 +0530

    Create sumOfLeftLeaves.java

---
+++ 20-BinaryTree/sumOfLeftLeaves.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/sum-of-left-leaves/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        int ans = 0;
+        if (root == null) {
+            return 0;
+        } 
+
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            ans += root.left.val;
+        }
+
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right) + ans;
+    }
+}

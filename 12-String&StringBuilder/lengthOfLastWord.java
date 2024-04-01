62326f84325f1bab140386eb3b7c42b23aad6717
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2024-04-01 23:35:30 +0530

    Create lengthOfLastWord.java

---
+++ 12-String&StringBuilder/lengthOfLastWord.java
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/length-of-last-word/
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if(length > 0) {
+                break;
+            }
+        }
+        return length;
+    }
+}

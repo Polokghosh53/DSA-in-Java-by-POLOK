a988a962202c145c9ded19fe0873417ab98092da
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2024-05-04 00:23:28 +0530

    Update compareVersion.java

--- 12-String&StringBuilder/compareVersion.java
+++ 12-String&StringBuilder/compareVersion.java
@@ -3,13 +3,13 @@ class Solution {
     public int compareVersion(String version1, String version2) {
         int i =0;
         int j =0;
-        while (i < version1.length || j < version2.length) {
+        while (i < version1.length() || j < version2.length()) {
             int num1 = 0;
             int num2 = 0;
             while (i < version1.length() && version1.charAt(i) != '.') {
                 num1 = num1 * 10 + (version1.charAt(i++) - '0');
             }
-            while (j < version2.length() && version2.charAt(i) != '.') {
+            while (j < version2.length() && version2.charAt(j) != '.') {
                 num2 = num2 * 10 + (version2.charAt(j++) - '0');
             }
             if (num1 < num2) {

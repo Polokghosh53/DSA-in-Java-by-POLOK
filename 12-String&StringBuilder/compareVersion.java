0690f369a77ba3581513be3a21afcc4c607b827c
Polok Ghosh <57084217+Polokghosh53@users.noreply.github.com>
2024-05-04 00:19:44 +0530

    Create compareVersion.java

---
+++ 12-String&StringBuilder/compareVersion.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/compare-version-numbers/
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int i =0;
+        int j =0;
+        while (i < version1.length || j < version2.length) {
+            int num1 = 0;
+            int num2 = 0;
+            while (i < version1.length() && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + (version1.charAt(i++) - '0');
+            }
+            while (j < version2.length() && version2.charAt(i) != '.') {
+                num2 = num2 * 10 + (version2.charAt(j++) - '0');
+            }
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+            i++;
+            j++;
+        }
+        return 0;
+    }
+}
